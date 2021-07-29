6dba92e078cf5d64ab90bcdcdbcae6734bd2084d
JuCSMartin <julianacmsantos@gmail.com>
2021-07-29 11:36:12 -0300

    Atualização de injeção UserDetailsService

--- blogpessoal/src/main/java/br/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ blogpessoal/src/main/java/br/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -14,7 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private UserDetailsServiceImpl service;
+	private UserDetailsService service;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
